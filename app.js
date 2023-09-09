1e88f751f604abea0812f3047117298b4f501a3a
shameem frhn <135575555+shameemfrhn@users.noreply.github.com>
2023-09-09 12:45:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
+openModalBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
